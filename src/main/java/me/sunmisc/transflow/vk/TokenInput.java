bd56e6c1573f30f34582cde8eb431665d6588b63
sunmisc <jolyjdia@gmail.com>
2024-02-10 00:52:29 +0300

    local variables used in lambda expressions anti reflection

--- src/main/java/me/sunmisc/transflow/vk/TokenInput.java
+++ src/main/java/me/sunmisc/transflow/vk/TokenInput.java
@@ -8,6 +8,9 @@ import java.util.Optional;
 public class TokenInput extends EnvelopeInput {
     private static final String API_VERSION = "5.139";
 
+    // We do not allow the token to be “drained” through Reflection
+    // local variables used in lambda expressions
+    // are not stored in the compiled bytecode
     public TokenInput(String accessToken) {
         super(() -> Optional.of(String
                 .format("&access_token=%s&v=%s", accessToken, API_VERSION)
